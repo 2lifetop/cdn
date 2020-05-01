b6bb3ba8acb77b465e73ae2f4222f3a8fb894f82
2life <jiawen14th@gmail.com>
2020-05-02 03:38:50 +0800

    pic to 5 number

---
+++ png_rename.bat
@@ -0,0 +1,1 @@
+for %%i in (*.png)  do ren "%%i" https://cdn.jsdelivr.net/gh/2lifetop/cdn/pixiv/r18"%%i"
--- random.php
+++ random.php
@@ -1,5 +1,5 @@
 <?php
-$arr=file('pixiv.txt');
+$arr=file('random.txt');
 $n=count($arr)-1;
 for ($i=1;$i<=1;$i++){
 $x=rand(0,$n);
